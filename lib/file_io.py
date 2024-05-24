67bfe3489707dacac94aca5c08e3dd77fcfc68c8
gamze <gamzekandara@gmail.com>
2024-05-23 23:37:21 -0400

    Adds file_io.py

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    # Open the file in write mode with utf-8 encoding, 'with' closes the file automatically.
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file:
+        # Write the content to the file
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a' , encoding='utf-8') as file:
+        return file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode='r', encoding='utf-8') as file:
+        return file.read()
+
